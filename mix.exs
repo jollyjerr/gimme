d1f349092f457c9c23c75205fdb7cba3c9f064db
Jeremiah Tabb <jollyjerr@gmail.com>
2025-11-26 09:36:15 -0700

    Add mix publish meta

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule Gimme.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "String/Atom QOL",
+      package: %{
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/jollyjerr/gimme"}
+      }
     ]
   end
 
